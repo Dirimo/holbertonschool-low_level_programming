1120fa7afa51cc29a3b16440596738e4b2b986ab
Dirimo <dirimoiriarte@gmail.com>
2025-02-16 23:23:43 +0100

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,13 +2,8 @@
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
 
-   	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
    	printf("Size of int: %zu byte(s)\n", sizeof(int));
     	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
     	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
@@ -16,5 +11,5 @@ int main(void)
    	printf("Size of double: %zu byte(s)\n", sizeof(double));
     	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 
-    return 0;
+    	return 0;
 }
