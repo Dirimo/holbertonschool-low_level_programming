7e5bd5a03a5863124943a239ad0ef41a4b23c2f7
Dirimo <dirimoiriarte@gmail.com>
2025-02-17 15:30:01 +0100

    alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    for (char ch = 'a'; ch <= 'z'; ++ch)
+    {
+        putchar(ch);
+    }
+    putchar('\n');  // Print a new line after the alphabet
+    return 0;
+}
