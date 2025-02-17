acdc56a1851de0bfb90e2f042f6704cb1b801a9d
Dirimo <dirimoiriarte@gmail.com>
2025-02-17 20:11:08 +0100

    New correction

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase,
+ *        followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     char alphabet[53];
     int i;
 
-    // Fill the array with lowercase and uppercase alphabets
-    for (i = 0; i < 26; i++) {
+    /* Fill the array with lowercase and uppercase alphabets */
+    for (i = 0; i < 26; i++)
+    {
         alphabet[i] = 'a' + i;
         alphabet[i + 26] = 'A' + i;
     }
-    alphabet[52] = '\n'; // Add newline character at the end
+    alphabet[52] = '\n'; /* Add newline character at the end */
 
-    // Print the array using putchar
-    for (i = 0; i < 53; i++) {
+    /* Print the array using putchar */
+    for (i = 0; i < 53; i++)
+    {
         putchar(alphabet[i]);
     }
 
-    return 0;
+    return (0);
 }
