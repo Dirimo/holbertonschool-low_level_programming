ad6ba9704684abaff5172d11c858997fd8781645
Dirimo <dirimoiriarte@gmail.com>
2025-02-17 15:18:04 +0100

    boucles

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,9 +5,9 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program generates a random number,
- * finds its last digit, and prints whether the last digit
- * is greater than 5, 0, or less than 6 and not 0.
+ * Description: This program generates a random number, finds its last
+ * digit, and prints whether the last digit is greater than 5, 0, or less
+ * than 6 and not 0.
  *
  * Return: Always 0 (Success)
  */
@@ -20,19 +20,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;  /* Generate a random number */
 	lastDigit = n % 10;        /* Get the last digit */
 
-	printf("Last digit of %d is %d ", n, lastDigit); /* Print initial part */
+	printf("Last digit of %d is %d ", n, lastDigit);
 
 	if (lastDigit > 5)
 	{
-		printf("and is greater than 5\n"); /* Print if last digit > 5 */
+		printf("and is greater than 5\n");
 	}
 	else if (lastDigit == 0)
 	{
-		printf("and is 0\n");           /* Print if last digit is 0 */
+		printf("and is 0\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n"); /* Print if last digit < 6 and != 0 */
+		printf("and is less than 6 and not 0\n");
 	}
 
 	return (0);
