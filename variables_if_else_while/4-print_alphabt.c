5c4ae7b75ba636a8a106b97090cc73c8eee5a9ee
Dirimo <dirimoiriarte@gmail.com>
2025-02-17 21:54:05 +0100

    that prints the alphabet in lowercase

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, excluding 'q' and 'e',
+ *        followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+
+    /* Print lowercase alphabet excluding 'q' and 'e' */
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e')
+        {
+            putchar(letter);
+        }
+    }
+
+    putchar('\n'); /* Add newline character at the end */
+
+    return (0);
+}
