67b108adc7649a511e820a0fc25d0345ad427f60
Dirimo <dirimoiriarte@gmail.com>
2025-02-17 21:48:34 +0100

    New correction

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,22 +8,19 @@
  */
 int main(void)
 {
-    char alphabet[53];
-    int i;
-
-    /* Fill the array with lowercase and uppercase alphabets */
-    for (i = 0; i < 26; i++)
-    {
-        alphabet[i] = 'a' + i;
-        alphabet[i + 26] = 'A' + i;
-    }
-    alphabet[52] = '\n'; /* Add newline character at the end */
-
-    /* Print the array using putchar */
-    for (i = 0; i < 53; i++)
-    {
-        putchar(alphabet[i]);
-    }
-
-    return (0);
+char alphabet[53];
+int i;
+/* Fill the array with lowercase and uppercase alphabets */
+for (i = 0; i < 26; i++)
+{
+alphabet[i] = 'a' + i;
+alphabet[i + 26] = 'A' + i;
+}
+alphabet[52] = '\n'; /* Add newline character at the end */
+/* Print the array using putchar */
+for (i = 0; i < 53; i++)
+{ 
+putchar(alphabet[i]);
+}
+return (0);
 }
