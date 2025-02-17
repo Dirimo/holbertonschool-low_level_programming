5ef06b76052730f401ec851075ea66877be8b2e9
Dirimo <dirimoiriarte@gmail.com>
2025-02-17 15:50:39 +0100

    prints the alphabet in lowercase and then in uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a small portion of the alphabet (lower and uppercase),
+ *        followed by a newline, demonstrating putchar usage.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    putchar('a'); // Prints lowercase 'a'
+    putchar('A'); // Prints uppercase 'A'
+    putchar('\n');  // Prints a newline
+
+    return (0);
+}
