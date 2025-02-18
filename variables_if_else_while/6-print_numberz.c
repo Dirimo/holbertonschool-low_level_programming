9ca0089285740334ad2f25ba11524a140e3d69cd
Dirimo <dirimoiriarte@gmail.com>
2025-02-18 10:03:58 +0100

    prints all single digit numbers of base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single-digit numbers from 0-9 using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10);
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
