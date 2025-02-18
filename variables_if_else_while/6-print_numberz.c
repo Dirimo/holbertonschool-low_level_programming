c5a16a2d4788bdf28dc36dd84106487c0936b872
Dirimo <dirimoiriarte@gmail.com>
2025-02-18 10:11:01 +0100

    prints all single digit numbers of base 10

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	int num = 0;
+    int num = 0;
 
-	while (num < 10);	
+    while (num < 10)
+    {
+        putchar(num + '0');
+        num++;
+    }
+    putchar('\n');
 
-	{
-		putchar(num + '0');
-		num++;
-	}
-	putchar('\n');
-
-	return (0);
+    return (0);
 }
