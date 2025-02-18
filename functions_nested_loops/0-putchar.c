ad14a01fcecbc3b4fe34a4fb20a58205f9b5a6ff
Dirimo <dirimoiriarte@gmail.com>
2025-02-18 18:02:00 +0100

    print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
