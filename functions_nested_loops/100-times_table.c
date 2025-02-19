9454b4255ac4572d97c82e99798d3ede6fd6cfa8
Dirimo <dirimoiriarte@gmail.com>
2025-02-19 17:47:27 +0100

    prints n time table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -20,10 +20,47 @@ void print_times_table(int n)
 			product = i * j;
 
 			if (j == 0)
-				printf("%d", product);
+				print_number(product);
 			else
-				printf(", %3d", product);
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (product < 100)
+				{
+					_putchar(' ');
+				}
+				print_number(product);
+			}
 		}
-		printf("\n");
+		_putchar('\n');
 	}
+}
+
+/**
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to print.
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
