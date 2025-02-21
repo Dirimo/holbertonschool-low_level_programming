c20c03be3b14d67806d69fd1e387fbdea9908bc3
Dirimo <dirimoiriarte@gmail.com>
2025-02-21 15:59:27 +0100

    possible different combinations of three digits

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of three digits.
+ * Numbers must be separated by commas and a space.
+ * The two digits must be different
+ * Numbers should be printed in ascending order, with two digitsi
+ * You can only use `putchar` to print to the console.
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
 int main(void)
 {
 	int x;
