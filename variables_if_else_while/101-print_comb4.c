7250cc7181d1a5ef67c9392ca4bb463145604d3e
Dirimo <dirimoiriarte@gmail.com>
2025-02-21 15:52:47 +0100

    possible different combinations of three digits

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z); 
+				if (x != '7' || y != '8' || z != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
