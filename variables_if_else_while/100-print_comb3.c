301320f90dbe721de013eeb46a61116866df7d21
Dirimo <dirimoiriarte@gmail.com>
2025-02-21 11:30:39 +0100

    prints all possible different combinations of two digits

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two digits.
+ * Numbers must be separated by commas and a space.
+ * The two digits must be different
+ * Numbers should be printed in ascending order, with two digitsi
+ * You can only use `putchar` to print to the console.
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
+int main(void)
+{
+        int x;
+	int y;
+
+        for (x = 0; x < 10; x++)
+ 
+	for (y = 0; y < 10; y++)
+      
+                {
+                        putchar(',');
+                        putchar(' ');
+                }
+        }
+        putchar('\n');
+        return (0);
+}
