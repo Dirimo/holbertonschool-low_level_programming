0a236880f5739b49de84785005227babb49b94a8
Dirimo <dirimoiriarte@gmail.com>
2025-02-21 12:52:53 +0100

    prints all possible different combinations of two digits

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,18 +12,23 @@
  */
 int main(void)
 {
-        int x;
+	int x;
 	int y;
 
-        for (x = 0; x < 10; x++)
- 
-	for (y = 0; y < 10; y++)
-      
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x != '8' && y != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
