ce2988f1ca588c0e7829f1e22f956424de0b8bd9
Dirimo <dirimoiriarte@gmail.com>
2025-02-23 20:11:15 +0100

    101 quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-	char *message= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message= "and that piece of art is useful\ ' = '
+		'Dora Korpar, 2015-10-19\n';
+
 	int length = 0;
 
 	while (message[length] != '\0')
