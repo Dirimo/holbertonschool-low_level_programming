ca9b9d63171769a7a703cb47732db1863656101c
Dirimo <dirimoiriarte@gmail.com>
2025-02-23 19:59:21 +0100

    101 quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int length = 0;
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = 0;
 
-    while (message[length] != '\0')
-        length++;
+	while (message[length] != '\0')
+		length++;
 
-    write(2, message, length);
+	write(2, message, length);
 
-    return 1;
+	return 1;
 }
