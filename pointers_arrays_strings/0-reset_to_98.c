6f3276433a6afd8fa2dc9dbf1b10057570426302
Dirimo <dirimoiriarte@gmail.com>
2025-02-24 14:07:28 +0100

    correction

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 
-int main(void) {
-    int my_number = 42;
-    int *ptr = &my_number;  // ptr now points to my_number
-
-    printf("Before: my_number = %d\n", my_number); // Output: Before: my_number = 42
-
-    reset_to_98(ptr);  // Call the function, passing the pointer
-
-    printf("After: my_number = %d\n", my_number);  // Output: After: my_number = 98
-
-    return (0);
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98
+ * @n: Pointer to an integer
+ *
+ * Description: This function takes a pointer to an int as a parameter
+ * and updates the value it points to to 98.
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
 }
+
