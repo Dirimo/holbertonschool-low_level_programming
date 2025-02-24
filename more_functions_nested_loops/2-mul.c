cced60ee050c53e7bbd97e2070e8668eceb8a22e
Dirimo <dirimoiriarte@gmail.com>
2025-02-24 21:58:03 +0100

    multiplier deux elements

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Write a function that multplies
+ * two integers
+ * 
+ *
+ */
+
+int mul(int a, int b);
+{
+	int a = 9
+	int b = 7
+
+	printf( "a * b \n");
+
+	return (0);
+}
