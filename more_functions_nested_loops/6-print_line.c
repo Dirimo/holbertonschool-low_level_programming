704c2169140338477541cd6d775c74e7516ba426
Dirimo <dirimoiriarte@gmail.com>
2025-02-25 14:02:18 +0100

    prints lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *Prototype: void print_line(int n);
+ *You can only use _putchar function to print
+ *Where n is the number of times the character _ should be printed
+ *The line should end with a \n
+ *If n is 0 or less, the function should only print \n
+ *
+ */
+
+void print_line(int n);
+{
+
+	int i;
+
+	for ( i = 0 ; i < n ; i++)
+
+		putchar("_");
+
+	putchar("\n")
+}
