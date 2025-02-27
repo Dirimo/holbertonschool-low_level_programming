1c0d12c9345a5fb07b8dd721571d983c0f5d428b
Dirimo <dirimoiriarte@gmail.com>
2025-02-27 13:59:01 +0100

    convert a string to an int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,27 @@
 #include "main.h"
 
 /**
- * Function that convert sting to an int
- * number in the str can be preceded by infinite number of char
- * take acount - and + signs before the number
- * Function must return 0
- * not allowed to use @long, new var de type arr and hard-code special values
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
  *
- * Will use -fsanitize=signed-integer-overflow gcc flag to compile
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
-
 	int sign = 1;
-	int result = 0;
-	int i = 0;
+	unsigned int num = 0;
 
-	/* Handle leading whitespace and signs */
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-			s[i] == '\r' || s[i] == '\f' || s[i] == '\v')
-		i++;
-
-	while (s[i] == '+' || s[i] == '-')
-	{
-		if (s[i] == '-')
+	do {
+		if (*s == '-')
 			sign *= -1;
-		i++;
-	}
 
-	/* Convert digits to integer */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		/* Check for potential overflow before multiplying */
-		if (result > (2147483647 / 10) || (result == (2147483647 / 10) && (s[i] - '0') > 7))
-		{
-			if (sign == 1)
-				return (2147483647);
-			else
-				return (-2147483648);
-		}
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-	return (sign * result);
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
