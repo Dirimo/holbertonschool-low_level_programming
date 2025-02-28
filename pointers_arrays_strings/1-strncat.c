079e74032b318b37b4eb73b3222812656b0b75fd
Dirimo <dirimoiriarte@gmail.com>
2025-02-28 11:24:03 +0100

    function that copies a string

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: Pointer to the destination string.
- * @src: Pointer to the source string.
+ * _strncat - Concatenates two strings, using at most n bytes from src.
+ * @dest: Pointer to the destination string.  The destination buffer
+ *        where the src string will be appended.  It must be large
+ *        enough to hold the result.
+ * @src: Pointer to the source string.  The string to be appended to
+ *       the dest string.
+ * @n: The maximum number of bytes to be appended from src.
+ *     If src is shorter than n bytes, the entire src string is appended.
  *
- * Return: A pointer to the resulting string `dest`.
+ * Return: A pointer to the resulting string dest.
+ *         This pointer is the same as the original dest pointer.
  */
 char *_strncat(char *dest, char *src, int n)
 {
