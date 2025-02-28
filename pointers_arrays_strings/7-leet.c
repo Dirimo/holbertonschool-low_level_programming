a0d27f644679b6a38d7c133547b47d890bfa2c12
Dirimo <dirimoiriarte@gmail.com>
2025-02-28 13:41:18 +0100

    that encodes a string into1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * leet - encodes a sting to 1337
- * #str: the string to be encoded
+ * leet - Encodes a string into "leet" (1337).
+ * @str: Pointer to the string to be encoded.
  *
- * return a pointer to the encoded string
+ * Return: Pointer to the encoded string.
  */
 char *leet(char *str)
 {
 	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L','?','E','A','?','?','T'};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (str[indx1] == leet [indx2]	||
+			if (str[indx1] == leet[indx2]	||
 					str[indx1] - 32 == leet[indx2])
 				str[indx1] = indx2 + '0';
 		}
