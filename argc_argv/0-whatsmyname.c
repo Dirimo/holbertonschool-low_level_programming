1fe7f1d1a6f1be3191877ab08e25bdfb04bc05a0
Dirimo <dirimoiriarte@gmail.com>
2025-03-07 13:28:01 +0100

    that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Suppress unused variable warning */
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
