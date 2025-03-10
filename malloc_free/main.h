dcb67d29d3d2c1f6e5dcfba3eab3a4b5815afd91
Dirimo <dirimoiriarte@gmail.com>
2025-03-10 14:01:49 +0100

    creates an array of chars, and initializes it with a specific char

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,7 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -8,4 +9,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
 
-#endif
+#endif /* MAIN_H */
