3e6f0901917b3f8c65bdc46085efbc3e30270559
Dirimo <dirimoiriarte@gmail.com>
2025-03-14 13:11:54 +0100

    define a new type

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+/**
+ * struct dog - Structure representing a dog.
+ *
+ * @name: The name of the dog (char *).
+ * @age: The age of the dog (float).
+ * @owner: The owner's name (char *).
+ *
+ * Description: This structure defines the basic
+ * attributes of a dog, including its name, age,
+ * and the name of its owner.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
