bd5a3f8810b96fdf23de0519c32ba8bdece43bf3
Dirimo <dirimoiriarte@gmail.com>
2025-04-03 13:55:33 +0200

    adding my task tree to eight

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: A pointer to the head of the list.
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	dlistint_t *next;
+
+	current = head;
+
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
