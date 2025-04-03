51c60703095c38908ab5834150d9775b2e5c9a24
Dirimo <dirimoiriarte@gmail.com>
2025-04-03 10:16:02 +0200

    correction of my code in task0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
-		count++;
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
 		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
